ca17b58b52ce36720e325713a8bdb649d9985230
Jack-Development <shiltonjack@gmail.com>
2021-07-31 21:00:52 +0100

    Reformatted to run from input and edited what is produced to be useable

--- convert.py
+++ convert.py
@@ -2,25 +2,51 @@ import re
 import sys
 
 
+class Strategy:
+    def __init__(self, name, exchange, entry, exit):
+        self.name = name
+        self.exchange = exchange
+        self.algorithm = {
+            'entry': entry,
+            'exit': exit
+        }
+
+
+default = Strategy("name", "exchange", "entry", "exit")
+
+
 def convert(text):
     if re.match('(Buy|Sell)\s(when)\s(crossover|crossunder)[(][.-^]*,[0-9]*[)]', text):
-        # Defines action
-        if re.match('^Buy', text):
-            function = 'buy()'
-        else:
-            function = 'sell()'
-        # Defines strategy
-        if text.__contains__('crossover'):
-            strategy = 'crossover'
-        else:
-            strategy = 'crossunder'
-
         # Finds market and amount
         bracket = re.search('(Buy|Sell)\s(when)\s(crossover|crossunder)[(](.*?)[)]', text)
         parameter = bracket.group(4).split(',')
+        default.exchange = parameter[0]
 
-        # Returns details
-        return function + ' ' + parameter[1] + ' ' + parameter[0] + ' ' + strategy
+        # Defines strategy
+        if text.__contains__('crossover'):
+            strategy = "indi[i:i+1]['Close'] ARROW AMOUNT and crossover==True"
+            strategy = strategy.replace("AMOUNT", parameter[1])
+
+            # Defines action
+            if re.match('^Buy', text):
+                strategy = strategy.replace("ARROW", ">")
+                default.algorithm = {
+                    'entry': strategy,
+                    'exit': default.algorithm['exit']
+                }
+            else:
+                strategy = strategy.replace("ARROW", "<")
+                default.algorithm = {
+                    'entry': default.algorithm['entry'],
+                    'exit': strategy
+                }
+        else:
+            strategy = 'crossunder'
 
 
-print(convert(sys.argv[1]))
+while True:
+    inp = input()
+    if inp == "exit":
+        sys.exit()
+    convert(inp)
+    print(default.name, default.exchange, "['", default.algorithm['entry'], "']", "['", default.algorithm['exit'], "']")
